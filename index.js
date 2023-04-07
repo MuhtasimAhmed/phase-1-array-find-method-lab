11c01fea856e269bd19d50ffeac7927088ccad5e
Muhtasim <mts.ahmed.1010@gmail.com>
2023-04-06 23:39:59 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(array){
+    let foundYear = array.find(findWin)
+    if (typeof foundYear === 'undefined'){
+      return foundYear
+    } else {
+      return foundYear.year
+    }
+        
+    }
+function findWin(object){
+    return object.result === "W"
+}
+
